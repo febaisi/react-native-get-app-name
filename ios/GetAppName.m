a450a2120762cb6bd63fd0b41f0d672dc792ccbd
Baisi, Felipe <felipe.baisi@lexisnexisrisk.com>
2021-02-16 15:44:11 -0500

    Improving iOS call

--- ios/GetAppName.m
+++ ios/GetAppName.m
@@ -6,8 +6,14 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(getAppName:(RCTResponseSenderBlock)callback)
 {
-    NSString *appName = [[[NSBundle mainBundle] infoDictionary] objectForKey:(id)kCFBundleNameKey];
+    NSString *appName = [self getAppName];
     callback(@[[NSString stringWithFormat: @"%@", appName]]);
+}
+
+- (NSString *) getAppName {
+    NSString *displayName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"];
+    NSString *bundleName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleName"];
+    return displayName ? displayName : bundleName;
 }
 
 @end
